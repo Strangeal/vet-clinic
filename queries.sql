9fed87cf9070f6d11aa87d4b11e3e50aa6c7212d
strangeal <bajerijustice@gmail.com>
2022-12-06 09:34:32 -0500

    query and update

--- queries.sql
+++ queries.sql
@@ -2,24 +2,49 @@
 
 -- Find all animals whose name ends in "mon"
 SELECT * FROM animals WHERE name LIKE '%mon'
-
--- List the name of all animals born between 2016 and 2019.
 SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-
--- List the name of all animals that are neutered and have less than 3 escape attempts
 SELECT name FROM animals WHERE neutered IS true AND escape_attempts < 3;
-
--- List the date of birth of all animals named either "Agumon" or "Pikachu"
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-
--- List name and escape attempts of animals that weigh more than 10.5kg
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-
--- Find all animals that are neutered
 SELECT * FROM animals WHERE neutered = true;
-
--- Find all animals not named Gabumon
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
-
--- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+-- QUERY AND UPDATE TABLE
+-- Transaction 1
+BEGIN; 
+UPDATE animals SET species = 'unspecified';
+ROLLBACK;
+
+-- Transaction 2
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+COMMIT;
+
+-- Transaction 3
+-- Delete from table in a transaction
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+-- Transaction 4
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP1;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+-- Answer Queries
+SELECT COUNT(*) AS total_animal_count FROM animals;
+SELECT * FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name VARCHAR(30),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight DECIMAL
+    weight_kg DECIMAL,
+    species VARCHAR(50),
 );
