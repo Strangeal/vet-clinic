00aa40874b2bd437eb40c88f50635040a635235d
strangeal <bajerijustice@gmail.com>
2022-12-06 11:14:34 -0500

    Add reviewer chnages

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(30),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
-    species VARCHAR(50),
 );
+
+ALTER TABLE animals ADD species VARCHAR(50);
